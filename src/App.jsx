1302f7227cbf5b6cad9f54a33f5d6c826d2e60f1
JoozKelly <joozkelly12@gmail.com>
2025-04-14 14:28:58 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 import { Home, About, Projects, Thesis, WA, CE, OE, Contact, Legal, } from "./pages";
